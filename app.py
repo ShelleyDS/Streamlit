3467f5807c89d258633dd229b4168672e4359c22
ShelleyDS <131091257+ShelleyDS@users.noreply.github.com>
2023-04-18 06:20:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,20 @@ This app tell you which number is bigger out of the three input given
 
 st.header('User Input')
 
-def user_input_features():
-    num1 = st.number_input("num1")
-    num2 = st.number_input("num2")
-    num3 = st.number_input("num3")
+num1 = st.number_input("1st Number")
+num2 = st.number_input("2nd Number")
+num3 = st.number_input("3rd Number")
   
-    data = {'1st Number': num1,
-            '2nd Number': num2,
-            '3rd Number': num3,
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
 
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df.to_dict())
-
-st.write(df['1st Number'])
+st.write(num1)
 
 st.subheader('Max Value is')
-if (df['1st Number'] > df['2nd Number'] and df['1st Number'] > df['3rd Number']):
-    st.write(df['1st Number'])
-elif df['2nd Number'] > df['3rd Number']:
-    st.write(df['2nd Number'])
+if (num1 > num2 and num1 > num3):
+    st.write(num1)
+elif num2 > num3:
+    st.write(num2)
 else :
-    st.write(df['3rd Number'])
+    st.write(num3)
 #st.write(prediction)
 
 st.subheader('Thanks for using the app')
